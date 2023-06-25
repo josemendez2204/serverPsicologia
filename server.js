099f22fc9ac60b3fb9b385e37eaafb4a8f67e76c
josemendez2204 <7716955+josemendez2204@users.noreply.github.com>
2023-06-25 18:26:03 +0200

    vercel config test

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 const { body, validationResult } = require("express-validator");
 const cors = require("cors");
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 const port = 3001;
-require('dotenv').config();
+require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
@@ -23,32 +23,30 @@ app.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-  const { firstName, lastName, tel, email, message } = req.body;
+    const { firstName, lastName, tel, email, message } = req.body;
 
-  let mailTransporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.PASSWORD
-    }
-  });
-  
-  let mailDetails = {
-    from: `${email}`, 
-    to: 'psic.andreacstantinoweb@gmail.com',
-    subject: `${firstName} ${lastName}`,
-    text: `${tel} ${message}`
-  };
-  
-  mailTransporter.sendMail(mailDetails, function(err, data) {
-    if(err) {
-      console.log('Error Occurs');
-    } else {
-      console.log('email sent');
-    }
-  });
-  
+    let mailTransporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+      },
+    });
 
+    let mailDetails = {
+      from: `${email}`,
+      to: "psic.andreacstantinoweb@gmail.com",
+      subject: `${firstName} ${lastName}`,
+      text: `${tel} ${message}`,
+    };
+
+    mailTransporter.sendMail(mailDetails, function (err, data) {
+      if (err) {
+        console.log("Error Occurs");
+      } else {
+        console.log("email sent");
+      }
+    });
 
     res.send("Data received successfully");
   }
